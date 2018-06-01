907fa270ac48387e08b88afdb35e9ff906255b88
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-06-01 22:29:27 +0000

    automatically backed up by learn

--- app/models/heroes.rb
+++ app/models/heroes.rb
@@ -2,8 +2,12 @@ class Hero
 
   attr_accessor :name, :power, :biography
 
+  HEROES = []
+
   def initialize(details)
     @name = details[:name]
     @power = details[:power]
     @biography = details[:biography]
+
+    
 end #class Hero
